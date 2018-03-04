d90714bfe7e895f0465d6b7b6351651d5810766e
parthiv.patel <parthiv.patel@metrosystems-external.net>
2018-03-04 19:40:14 +0530

    changes to test

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./build'));
+
+var host = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
+app.listen(port,host,function () {
+  console.log( "Listening on " + host + ", port " + port )
+});
